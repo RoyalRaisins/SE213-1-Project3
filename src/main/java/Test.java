f20ebb6c6992eb2861911ea1995af9c6d71795c7
RoyalRaisins <215783288@qq.com>
2020-06-13 15:34:32 +0800

    Five functions of Baidu homepage tested

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+import org.openqa.selenium.interactions.CompositeAction;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,11 +21,25 @@ public class Test {
 
         Actions action = new Actions(driver);
         action.moveToElement(driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[4]/span"))).perform();
+        Thread.sleep(1000);
+        action.release();
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[4]/div/div/a[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[6]/div/div/div/div[1]/form/div/ul/li[1]/span[2]/span[2]/span/input")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[6]/span/i")).click();
+        Thread.sleep(1000);
+        //floating select button
 
         WebElement searchBox = driver.findElement(By.id("kw"));
         searchBox.sendKeys("小坦克 博客园");
+        Thread.sleep(1000);
+        //Text entering
+
         WebElement searchButton = driver.findElement(By.id("su"));
         searchButton.click();
+        //Searching
+
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[3]/div[1]/h3/a")).click();
         Thread.sleep(1000);
@@ -37,10 +51,11 @@ public class Test {
             driver.switchTo().window(winHandle);
             break;
         }
-        //Search and jump
+        //Redirection
+
         Thread.sleep(4000);
         driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[2]/a[2]")).click();
-        //searchButton.submit();
+        //Check whether redirection was successful
         Thread.sleep(3000);
         driver.close();
     }
